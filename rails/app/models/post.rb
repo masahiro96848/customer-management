3c0340b246c2925f549dac1e24638ad123c0017a
masahiro96848 <feetwcpic9436@gmail.com>
2024-08-28 10:33:08 +0900

    favoriteテーブル、モデルを作成。リレーションも追加。

--- rails/app/models/post.rb
+++ rails/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   mount_uploader :image_url, ImageUploader
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_by_users, through: :favorites, source: :user
   belongs_to :user
 
   validates :title, presence: true
